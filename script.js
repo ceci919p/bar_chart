34f0d59c45e50a1273d03966aea797c549df3a71
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2022-02-09 22:04:33 +0100

    design added

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+window.addEventListener("load", init);
+
+const model = [
+  8, 12, 32, 4, 13, 24, 23, 22, 1, 0, 30, 7, 3, 6, 3, 19, 24, 29, 14, 16, 3, 1,
+  9, 8, 30, 12, 0, 30, 7, 3, 6, 3, 19, 24, 29, 14, 4, 1, 18, 1,
+];
+
+/* const queueSize = getNumberOfCustomers; */
+
+function init() {}
+
+function loop() {}
+
+function getNumberOfCustomers() {
+  //FAKE: gives a random number of guests
+  return Math.floor(Math.random() * 32);
+}
+
+function displayData() {}
+
+function modifyModel() {}
