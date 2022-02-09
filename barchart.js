a50eac80e5359a40d6cbd98de944c00d1e848346
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2022-02-09 20:19:55 +0100

    first commit

---
+++ barchart.js
@@ -0,0 +1,17 @@
+"use strict";
+
+window.addEventListener("load", init);
+/* const queueSize = getNumberOfCustomers; */
+
+function init() {}
+
+function loop() {}
+
+function getNumberOfCustomers() {
+  /*   //FAKE: gives a complete random number
+  return Math.floor(Math.random() * 32); */
+}
+
+function displayData() {}
+
+function modifyModel() {}
